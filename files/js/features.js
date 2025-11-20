d7cf52e2ced192bd68192fa8c277c2797bba39e1
Yusuf Mert Turan <yusufmertturan@gmail.com>
2025-11-20 13:12:58 +0300

    refactor features.js and updated gizlilik.html

--- files/js/features.js
+++ files/js/features.js
@@ -44,12 +44,28 @@
         function applyColorScheme(){
             document.querySelector(".background").style.backgroundColor = (colorScheme === "dark") ? "rgb(30, 30, 30)" : "rgb(187, 187, 187)";
             document.getElementById("darkModeButton").innerHTML = (colorScheme === "dark") ? "light mode" : "dark mode";
-            
-            let straightTexts = document.querySelectorAll(".straightText");
 
-            if(straightTexts.length > 0){
-                straightTexts.forEach(straightText =>{
-                    straightText.style.color = (colorScheme === "dark") ? "white" : "black";
+            let pTexts = document.querySelectorAll("p, .straightText, .interactiveText" );
+
+            if(pTexts.length > 0){
+                pTexts.forEach(pText =>{
+                    pText.style.color = (colorScheme === "dark") ? "white" : "black";
+                });
+            }
+
+            let hTexts = document.querySelectorAll("h1, h2, h3");
+
+            if(hTexts.length > 0){
+                hTexts.forEach(hText =>{
+                    hText.style.color = (colorScheme === "dark") ? "white" : "black";
+                });
+            }
+
+            let liTexts = document.querySelectorAll("li");
+
+            if(liTexts.length > 0){
+                liTexts.forEach(liText =>{
+                    liText.style.color = (colorScheme === "dark") ? "white" : "black";
                 });
             }
             
@@ -61,15 +77,8 @@
                 });
             }
 
-            let interactiveTexts = document.querySelectorAll(".interactiveText");
-
-            if(interactiveTexts.length > 0){
-                interactiveTexts.forEach(iText =>{
-                    iText.style.color = (colorScheme === "dark") ? "white" : "black";
-                });
-            }
             // index.html project cards
-            let projectCards = document.querySelectorAll(".project-card");
+            let projectCards = document.querySelectorAll(".project-card, .project-card-P");
 
             if(projectCards.length > 0){
                 projectCards.forEach(projectCard =>{
@@ -78,30 +87,11 @@
                 });
             }
 
-            let projectCardsP = document.querySelectorAll(".project-card p");
+            let projectCardsP = document.querySelectorAll(".project-card p, .project-card-P p");
             
             if(projectCardsP.length > 0){
                 projectCardsP.forEach(projectCard =>{
                     projectCard.style.color = (colorScheme === "dark") ? "rgb(255, 255, 255)" : "rgb(85, 85, 85)";
-                });
-            }
-            
-            // projects.html project cards
-            let projectsProjectCards = document.querySelectorAll(".project-card-P");
-
-            if(projectsProjectCards.length > 0){
-                projectsProjectCards.forEach(projectCard =>{
-                projectCard.style.backgroundColor = (colorScheme === "dark") ? "rgb(128, 128, 128)" : "rgb(255, 255, 255)";
-                projectCard.style.borderColor = (colorScheme === "dark") ? "rgb(255, 255, 255)" : "rgb(204, 204, 204)";
-                });
-            }
-            
-            let projectsProjectCardsP = document.querySelectorAll(".project-card-P p");
-            
-            if(projectsProjectCardsP.length > 0){
-                projectsProjectCardsP.forEach(projectCard =>{
-                    projectCard.style.color = (colorScheme === "dark") ? "rgb(255, 255, 255)" : "rgb(85, 85, 85)";
-                
                 });
             }
         }
