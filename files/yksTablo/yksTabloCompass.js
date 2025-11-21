d793d4ffe779b9b89ec63067918096c0344c59d6
Yusuf Mert Turan <yusufmertturan@gmail.com>
2025-11-22 01:18:35 +0300

    test

--- files/yksTablo/yksTabloCompass.js
+++ files/yksTablo/yksTabloCompass.js
@@ -127,7 +127,7 @@ function drawAllIndividuals(){
     drawCanvasThings();
     for(let i = 0; i < individuals.length; i++){
         if(i === hoveredIndividual || i === clickedIndividual){
-            if(i === clickedIndividual) writeSamePlaceList(i.x, i.y);
+            if(i === clickedIndividual) writeSamePlaceList(individuals[i].x, individuals[i].y);
             drawIndividual(individuals[i], true);
             drawTextbox(individuals[i]);
         } else{
