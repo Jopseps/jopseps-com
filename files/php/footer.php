46efda40e1ecafbcf6ca49089823519d78d0f56b
Yusuf Mert Turan <yusufmertturan@gmail.com>
2026-01-18 15:49:19 +0300

    php test

---
+++ files/php/footer.php
@@ -0,0 +1,24 @@
+<footer class="site-footer">
+    <div class="footer-content" id="footer">
+        <div class="footer-section">
+            <h4>About Me</h4>
+            <p>Yusuf Mert</p>
+            </div>
+                <div class="footer-section">
+                    <h4>Contact</h4>
+                    <p>E-mail: <a href="&#109;&#97;&#105;&#108;&#116;&#111;&colon;&#106;&#111;&#112;&#115;&#101;&#112;&#115;&#109;&#101;&#114;&#116;
+                        &commat;&#103;&#109;&#97;&#105;&#108;&period;&#99;&#111;&#109;">jopsepsmert@gmail.com</a></p>
+                </div>
+                <div class="footer-section">
+                    <h4>Links</h4>
+                <ul>
+                    <li><a href="../index.html">Home</a></li>
+                    <li><a href="../projects.html">Projects</a></li>
+                    <li><a href="../contacts.html">Contacts</a></li>
+                </ul>
+            </div>
+        </div>
+    <div class="footer-bottom">
+        © 2025 Jopseps. All rights reserved.
+    </div>
+</footer>
